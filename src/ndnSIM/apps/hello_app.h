8bc7d51ceeb0ad5edadfc1421e5dd3c01c0d5ec6
Priyesh Vakayil <pvakayil@nvidia.com>
2016-08-05 20:24:03 -0700

    hello app files added

---
+++ src/ndnSIM/apps/hello_app.h
@@ -0,0 +1,50 @@
+// hello-app.h
+
+#ifndef HELLO_APP_H_
+#define HELLO_APP_H_
+
+#include "ns3/ndn-app.h"
+#include "../../../scratch/myndn.h"
+
+namespace ns3 {
+
+class HelloApp : public ndn::App
+{
+public:
+  // register NS-3 type "HelloApp"
+  static TypeId
+  GetTypeId ();
+
+  // (overridden from ndn::App) Processing upon start of the application
+  virtual void
+  StartApplication ();
+
+  // (overridden from ndn::App) Processing when application is stopped
+  virtual void
+  StopApplication ();
+
+  // (overridden from ndn::App) Callback that will be called when Interest arrives
+  virtual void
+  OnInterest (Ptr<const ndn::Interest> interest);
+
+  // (overridden from ndn::App) Callback that will be called when Data arrives
+  virtual void
+  OnData (Ptr<const ndn::Data> contentObject);
+
+  Ptr<NdnNode>
+  GetNdnNode(Ptr<Node> curNode);
+
+  void 
+  SendHello();
+
+  Ptr<Face> 
+  GetFace(unsigned firstNodeId, unsigned secondNodeId);
+
+private:
+  void
+  SendInterest ();
+};
+
+} // namespace ns3
+
+#endif // HELLO_APP_H_
